f21606d6764ed33510f0a0a3916d9572a126b782
Fridah-Wothay <wothayafridahkamau@gmail.com>
2024-03-29 11:44:00 +0300

    add file

---
+++ functions.js
@@ -0,0 +1,29 @@
+let arr = ["Bus","car", "nissan",]
+function arrSort(arr){
+    arr.sort();
+    arr.reverse();
+    return arr;
+
+}
+console.log(arrSort(arr));
+
+//2
+
+
+var e =[1,2,3,4,5 -1, -2-3]
+function Array(){
+    let neg = f.filter((element) =>
+    element<0);
+    let zero = f.filter((element) =>
+    element==0);
+    return {
+    
+        neg: neg,
+        zero:zero
+    }
+
+}
+console.log(array(e));
+
+//4
+
